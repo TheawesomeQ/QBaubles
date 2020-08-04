b3f0987ad01bafa8fbc1f8e753a82e3abae89497
Q <q@DESKTOP-PJMFU3I.localdomain>
2020-08-03 22:09:54 -0400

    Completely rethought the event process. Instead of seeking information from the item, the arrow searches the equip slots of the target player for the shieldingring. This greatly simplifies attributes of both classes.

--- src/main/java/com/github/QBaubles/items/ShieldingRing.java
+++ src/main/java/com/github/QBaubles/items/ShieldingRing.java
@@ -5,61 +5,63 @@ package com.github.QBaubles.items;
 // - Change the value from BELT to RING below
 // - Change the name of the item in asssets.qbaubles.lang/en_us.lang
 import baubles.api.BaubleType;
+import baubles.api.BaublesApi;
 import baubles.api.IBauble;
+import baubles.api.cap.IBaublesItemHandler;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ShieldingRing extends ItemBasic implements IBauble {
-	protected boolean shielded;
-	protected EntityLivingBase holder;
-
 	public ShieldingRing(String name) {
 		super(name);
-		MinecraftForge.EVENT_BUS.register(new ShieldingEventHandler(this));
+		MinecraftForge.EVENT_BUS.register(new ShieldingEventHandler());
 	}
 
 	@Override
 	public BaubleType getBaubleType(ItemStack arg0) {
-		return BaubleType.RING;
+		return BaubleType.BELT;
 	}
 
 	public void onEquipped(ItemStack itemstack, EntityLivingBase player) {
-		holder = player;
-		shielded = true;
-//		System.out.println("ShieldingRing equipped to player: " + player.getName());
 	}
 
 	public void onUnequipped(ItemStack itemstack, EntityLivingBase player) {
-		holder = null;
-		shielded = false;
 	}
-
-
 
 }
 
 class ShieldingEventHandler {
-	private ShieldingRing shield_ring;
-
-	public ShieldingEventHandler(ShieldingRing shieldingRing) {
-		shield_ring = shieldingRing;
-	}
-
 	@SubscribeEvent
 	public void deflectArrow(ProjectileImpactEvent.Arrow event) {
-//		if (shield_ring.holder!=null) {System.out.println("Deflect Arrow Event Triggered!!! Player: "+shield_ring.holder.getName());}
-		if ((shield_ring.holder != null) && (event.isCancelable())) {
-			// Arrows are bounced if damage = 0. So, set damage to 0 if equipped & sneaking
-			// Old approach canceled the impact and caused arrows to fly right through.
-			if(shield_ring.shielded && shield_ring.holder.isSneaking()) {
-			//event.setCanceled(true);
-			event.getArrow().setDamage(0);}
-			System.out.print(shield_ring.shielded);
-			// System.out.print(shield_ring.getHolder().isSneaking());
-
+		Entity target = event.getRayTraceResult().entityHit;
+		if(target != null) {
+		if(target instanceof EntityPlayer) {
+			IBaublesItemHandler bauble = BaublesApi.getBaublesHandler((EntityPlayer) target);
+			boolean isEquipped = false;
+			for (int i=0; i < bauble.getSlots(); i++) {
+				if(bauble.getStackInSlot(i).getItem() instanceof ShieldingRing) {
+					isEquipped = true;
+					break;
+				}
+			}
+			if(isEquipped && target.isSneaking()) {
+				event.getArrow().setDamage(0);
+			}
+		}
 		}
 	}
 }
+
+
+
+/*
+ * Rethink this event handling.
+ * 
+ * Event will trigger on every arrow hit. It will scan the target's bauble slots
+ * for the ring and set the arrow damage to zero if it finds it.
+ * */
