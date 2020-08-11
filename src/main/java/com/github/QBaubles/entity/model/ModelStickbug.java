1632239c76d9b4e083c5105566dfb21cae13d2a0
Q <q@DESKTOP-PJMFU3I.localdomain>
2020-08-11 12:34:43 -0400

    Fixed legs and leg rotation origins for stickbug

--- src/main/java/com/github/QBaubles/entity/model/ModelStickbug.java
+++ src/main/java/com/github/QBaubles/entity/model/ModelStickbug.java
@@ -4,10 +4,9 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
-
-// Made with Blockbench 3.6.5
-// Exported for Minecraft version 1.12
-// Paste this class into your mod and generate all required imports
+//Made with Blockbench 3.6.5
+//Exported for Minecraft version 1.12
+//Paste this class into your mod and generate all required imports
 
 
 public class ModelStickbug extends ModelBase {
@@ -24,28 +23,28 @@ public class ModelStickbug extends ModelBase {
 		textureHeight = 32;
 
 		lf_leg = new ModelRenderer(this);
-		lf_leg.setRotationPoint(0.0F, 17.0F, 0.0F);
-		lf_leg.cubeList.add(new ModelBox(lf_leg, 0, 0, 9.0F, -1.0F, -4.0F, -9, 1, 1, 0.0F, false));
+		lf_leg.setRotationPoint(0.0F, 17.0F, -4.0F);
+		lf_leg.cubeList.add(new ModelBox(lf_leg, 0, 0, 0.0F, -1.0F, 0.0F, 9, 1, 1, 0.0F, false));
 
 		lm_leg = new ModelRenderer(this);
-		lm_leg.setRotationPoint(0.0F, 24.0F, 5.0F);
-		lm_leg.cubeList.add(new ModelBox(lm_leg, 0, 0, 9.0F, -8.0F, -4.0F, -9, 1, 1, 0.0F, false));
+		lm_leg.setRotationPoint(0.0F, 17.0F, 1.0F);
+		lm_leg.cubeList.add(new ModelBox(lm_leg, 0, 0, 0.0F, -1.0F, 0.0F, 9, 1, 1, 0.0F, false));
 
 		lr_leg = new ModelRenderer(this);
-		lr_leg.setRotationPoint(0.0F, 24.0F, 10.0F);
-		lr_leg.cubeList.add(new ModelBox(lr_leg, 0, 0, 9.0F, -8.0F, -4.0F, -9, 1, 1, 0.0F, false));
+		lr_leg.setRotationPoint(0.0F, 17.0F, 6.0F);
+		lr_leg.cubeList.add(new ModelBox(lr_leg, 0, 0, 0.0F, -1.0F, 0.0F, 9, 1, 1, 0.0F, false));
 
 		rf_leg = new ModelRenderer(this);
-		rf_leg.setRotationPoint(-2.0F, 17.0F, 0.0F);
-		rf_leg.cubeList.add(new ModelBox(rf_leg, 0, 0, -9.0F, -1.0F, -4.0F, 9, 1, 1, 0.0F, false));
+		rf_leg.setRotationPoint(-2.0F, 17.0F, -4.0F);
+		rf_leg.cubeList.add(new ModelBox(rf_leg, 0, 0, -9.0F, -1.0F, 0.0F, 9, 1, 1, 0.0F, false));
 
 		rm_leg = new ModelRenderer(this);
-		rm_leg.setRotationPoint(-2.0F, 17.0F, 5.0F);
-		rm_leg.cubeList.add(new ModelBox(rm_leg, 0, 0, -9.0F, -1.0F, -4.0F, 9, 1, 1, 0.0F, false));
+		rm_leg.setRotationPoint(-2.0F, 17.0F, 1.0F);
+		rm_leg.cubeList.add(new ModelBox(rm_leg, 0, 0, -9.0F, -1.0F, 0.0F, 9, 1, 1, 0.0F, false));
 
 		rr_leg = new ModelRenderer(this);
-		rr_leg.setRotationPoint(-2.0F, 17.0F, 10.0F);
-		rr_leg.cubeList.add(new ModelBox(rr_leg, 0, 0, -9.0F, -1.0F, -4.0F, 9, 1, 1, 0.0F, false));
+		rr_leg.setRotationPoint(-2.0F, 17.0F, 6.0F);
+		rr_leg.cubeList.add(new ModelBox(rr_leg, 0, 0, -9.0F, -1.0F, 0.0F, 9, 1, 1, 0.0F, false));
 
 		bb_main = new ModelRenderer(this);
 		bb_main.setRotationPoint(0.0F, 24.0F, 0.0F);
